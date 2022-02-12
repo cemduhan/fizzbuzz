edd11d6b5c99c70bc84403c01d45786d0978c856
Cem Karagoz <cem.duhan@gmail.com>
2022-02-12 21:03:46 +0300

    added modulus check of 3

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -4,14 +4,23 @@
 * @returns {String|Array} Sum of a and b or an array that contains a, b and the sum of a and b.
 */
 function fizzbuzz(number){
+    var arr = []    
     if(number <= 0){
         return ['-1']
     }
     if(typeof(number) != typeof(0)){
         return ['-1']
     }
-
-    return [number.toString()]
+    for (let x=1 ; x<=number ; x+=1) {
+        let appends = ''
+        if(x % 3 === 0){
+            appends = 'fizz'
+        }else{
+            appends = x.toString()
+        }
+        arr.push(appends)
+    }
+    return arr
 }
 
 export default fizzbuzz
